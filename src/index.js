625da151d850c137544cacf04119df89c6e880bf
Sim Singh <45135485+Singh2s7@users.noreply.github.com>
2021-02-16 15:57:05 -0500

    Add rest of nav pages and create component folder

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Home } from './home';
-import { Chat } from './chat';
+import { Home } from './components/home';
+import { Chat } from './components/chat';
+import { Groups } from './components/groups';
+import { Explore } from './components/explore';
+import { Calender } from './components/calender';
+import { Settings } from './components/settings';
 
 const sidenavbar = (
   <div>
@@ -15,11 +19,20 @@ const sidenavbar = (
         </header>
         <Link to="/home">Home</Link>
         <Link to="/chat">Chat</Link>
+        <Link to="/groups">Groups</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/calender">Calender</Link>
+        <Link to="/settings">Settings</Link>
       </div>
+
       <div id="main">
         <Switch>
           <Route path="/home" component={Home}/>
           <Route path="/chat" component={Chat}/>
+          <Route path="/groups" component={Groups}/>
+          <Route path="/explore" component={Explore}/>
+          <Route path="/calender" component={Calender}/>
+          <Route path="/settings" component={Settings}/>
           <Route component={Home}/>
         </Switch>
       </div>
