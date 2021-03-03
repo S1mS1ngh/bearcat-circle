cfcdd6990c90bdcc08b8115c8269c6adaeb4ecf2
Sim Singh <45135485+Singh2s7@users.noreply.github.com>
2021-03-02 22:53:58 -0500

    Update Groups card and Explore styling

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,6 +6,8 @@ import {Container, Row, Col, Card, Button} from 'react-bootstrap';
 import HomeForum from "./home.forum";
 import AddGroup from "../../static/plus_button.svg";
 
+//Todo Improve CSS styling if time permits
+
 export default class Home extends React.Component {
     render() {
         return (
@@ -18,50 +20,64 @@ export default class Home extends React.Component {
                         </Col>
                         <Col>
                             <Row>
-                                <Card id="groupsCard">
+                                <Card className="groupsCard">
                                     <Card.Header id="groupsCardHeader">
                                         Groups
                                         <Button style={{padding: "0 0 0 0", backgroundColor: "transparent", borderColor:"transparent", marginLeft:"345px"}}>
                                             <img src={AddGroup} alt="Add Group" />
                                         </Button>
                                     </Card.Header>
-                                        <Container fluid>
-                                            <Row>
-                                                <Col>
-                                                    1
+                                        <Container style={{padding: "0", margin: "0", textAlign: "center"}}>
+                                            <Row style={{padding: "0", margin: "37px 15px 23px 15px", textAlign: "center"}}>
+                                                <Col style={{padding: "0", margin: "0 0 0 10px"}}>
+                                                    <Button id="groupsCardGroupsButton" style={{backgroundColor: "#F38181"}}>
+                                                        Calculus 1
+                                                    </Button>
                                                 </Col>
-                                                <Col>
-                                                    2
+                                                <Col style={{padding: "0", margin: "0 10px 0 0"}}>
+                                                    <Button id="groupsCardGroupsButton" style={{backgroundColor: "#8381F3"}}>
+                                                        Chemistry I
+                                                    </Button>
                                                 </Col>
                                             </Row>
-                                            <Row>
-                                                <Col>
-                                                    3
+                                            <Row style={{padding: "0", margin: "15px 15px 15px 15px", textAlign: "center"}}>
+                                                <Col style={{padding: "0", margin: "0 0 0 10px"}}>
+                                                    <Button id="groupsCardGroupsButton" style={{backgroundColor: "#FCE38A"}}>
+                                                        ENED I
+                                                    </Button>
                                                 </Col>
-                                                <Col>
-                                                    4
+                                                <Col style={{padding: "0", margin: "0 10px 0 0"}}>
+                                                    <Button id="groupsCardGroupsButton" style={{backgroundColor: "#95E1D3"}}>
+                                                        My Learning Community
+                                                    </Button>
                                                 </Col>
                                             </Row>
                                         </Container>
                                 </Card>
                             </Row>
                             <Row>
-                                <Card id="groupsCard">
+                                <Card className="exploreCard">
                                     <Card.Header id="groupsCardHeader">Explore</Card.Header>
-                                    <Container fluid>
+                                    <Container fluid style={{margin: "45px 0px 0px 25px"}}>
                                         <Row>
                                             <Col>
-                                                1
+                                                <Button>
+                                                    Find A Study Group
+                                                </Button>
                                             </Col>
                                         </Row>
                                         <Row>
                                             <Col>
-                                                2
+                                                <Button>
+                                                    Find A Study Buddy
+                                                </Button>
                                             </Col>
                                         </Row>
                                         <Row>
                                             <Col>
-                                                3
+                                                <Button style={{backgroundColor: "#30475E", color: "white"}}>
+                                                    View more
+                                                </Button>
                                             </Col>
                                         </Row>
                                     </Container>
