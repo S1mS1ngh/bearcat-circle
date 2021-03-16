6b86e465789b4d695d423d579aed6f4a31495ca5
grussra <51136411+grussra@users.noreply.github.com>
2021-03-16 12:40:01 -0400

    Added Forum Base
    Added input and post of forum

---
+++ frontend/src/components/Post/Post.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+
+import React from 'react';
+import './Post.css';
+
+const Post = (props) => (
+    <div className="panel panel-default post-body">
+        <div className="panel-body">
+            {
+                props.postBody.map((postPart, idx) => (
+                    <div key={idx}>{postPart}</div>
+                ))
+            }
+        </div>
+    </div>
+);
+
+export default Post;
--- frontend/src/components/home/home.forum.js
+++ frontend/src/components/home/home.forum.js
@@ -2,40 +2,33 @@ import React from 'react';
 import '../../css/home/home.forum.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ForumCard from "./forum.card";
+import ThreadDisplay from '../Post/ThreadDisplay';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 export default class HomeForum extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            items: [],
-            isLoaded: false,
-        };
-    }
 
-    componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        fetch('SampleForum.json')
-            .then(res => res.json())
-            .then(result => {
-                this.setState({
-                    isLoaded: true,
-                    items: result
-                });
-            });
+        const config =
+        {
+            apiKey: "AIzaSyDaj-viOsJb6Iibb8JChqPD7pAv3IJToS0",
+            authDomain: "bearcat-circle.firebaseapp.com",
+            databaseURL: "https://bearcat-circle-default-rtdb.firebaseio.com",
+            projectId: "bearcat-circle",
+            storageBucket: "bearcat-circle.appspot.com",
+            messagingSenderId: "343522068166",
+            appId: "1:343522068166:web:5352db95ea285e63c179c7",
+            measurementId: "G-C7Q90SCRL7"
+        };
+
+        this.app = firebase.initializeApp(config);
+        this.database = this.app.database();
     }
 
     render() {
-        const { items } = this.state;
-        if (!this.state.isLoaded) {
-            return <div>Loading ... </div>;
-        } else {
-            return (
-                <div className="forum">
-                    {items.map(item => (
-                        <ForumCard id = {item.id} title = {item.title} body = {item.body} />
-                    ))}
-                </div>
-            );
-        }
+        return (
+            <ThreadDisplay database={this.database} />
+        );
     }
 }
