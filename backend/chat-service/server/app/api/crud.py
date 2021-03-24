720fc1fe61ab3199fe92229e869d1041b4d9b80b
Giang Ta <thoanggiang98@gmail.com>
2021-03-24 01:37:29 -0400

    finish delete route for forum

--- backend/chat-service/server/app/api/crud.py
+++ backend/chat-service/server/app/api/crud.py
@@ -38,3 +38,17 @@ async def create_post(payload: PostIn, current_user: UserAuth) -> Post_Pydantic:
 async def get_all_post() -> List[Post_Pydantic]:
     posts = await Post_Pydantic.from_queryset(Post.all())
     return posts
+
+async def get_post_by_id(id: int) -> Union[dict, None]:
+    post = await Post.filter(id=id).first().values()
+    if post:
+        return post[0]
+    return None
+
+async def delete_post_by_id(id: int, current_user: UserAuth) -> Union[Post_Pydantic, None]:
+    post = await Post.filter(id=id).first()
+    user = await get_user(current_user.username)
+    if post.user_id == user['id']:
+        post = await Post.filter(id=id).first().delete()
+        return post
+    return None
--- backend/chat-service/server/app/api/forum.py
+++ backend/chat-service/server/app/api/forum.py
@@ -19,3 +19,26 @@ async def create_post(payload: PostIn, active_user: UserAuth = Security(get_curr
 @router.get('/all', response_model=List[Post_Pydantic])
 async def get_all_post() -> List[Post_Pydantic]:
     return await crud.get_all_post()
+
+@router.get('/{id}/', response_model=Post_Pydantic, status_code=200)
+async def get_one_post(id: int) -> Post_Pydantic:
+    post = await crud.get_post_by_id(id)
+    del post["updated_at"]
+    del post["created_at"]
+    del post["user_id"]
+    return Post_Pydantic(**post)
+
+@router.delete("/{id}/", response_model=Post_Pydantic)
+async def delete_post(id: int, active_user: UserAuth = Security(get_current_user)):
+    post = await crud.get_post_by_id(id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found or belong to current user")
+    
+    deleted_post = await crud.delete_post_by_id(id, active_user)
+    if not post:
+        raise HTTPException(status_code=401, detail="User not authorized to delete this")
+    
+    del post["updated_at"]
+    del post["created_at"]
+    del post["user_id"]
+    return Post_Pydantic(**post)
